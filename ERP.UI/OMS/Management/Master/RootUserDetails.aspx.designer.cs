059c34ef5ae9f63c40b17342fb1226c924d98f71
SanchitaINT <82153205+SanchitaINT@users.noreply.github.com>
2023-09-06 20:16:03 +0530

    commit

--- ERP.UI/OMS/Management/Master/RootUserDetails.aspx.designer.cs
+++ ERP.UI/OMS/Management/Master/RootUserDetails.aspx.designer.cs
@@ -4191,6 +4191,60 @@ namespace ERP.OMS.Management.Master
         protected global::DevExpress.Web.ASPxCheckBox chkShowAttendanceSummary;
 
         /// <summary>
+        /// divMenuShowAIMarketAssistant control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTableCell divMenuShowAIMarketAssistant;
+
+        /// <summary>
+        /// chkMenuShowAIMarketAssistant control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::DevExpress.Web.ASPxCheckBox chkMenuShowAIMarketAssistant;
+
+        /// <summary>
+        /// divUsbDebuggingRestricted control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTableCell divUsbDebuggingRestricted;
+
+        /// <summary>
+        /// chkUsbDebuggingRestricted control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::DevExpress.Web.ASPxCheckBox chkUsbDebuggingRestricted;
+
+        /// <summary>
+        /// divShowLatLongInOutletMaster control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTableCell divShowLatLongInOutletMaster;
+
+        /// <summary>
+        /// chkShowLatLongInOutletMaster control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::DevExpress.Web.ASPxCheckBox chkShowLatLongInOutletMaster;
+
+        /// <summary>
         /// btnUpdate control.
         /// </summary>
         /// <remarks>
